bd9b02818e2a97200dbe7fad2cc094bb4a26dc9e
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-10-18 18:30:23 +0500

    6

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -3,7 +3,7 @@ number = 7
 #Divide the number by 2 and assign number.
 number / 2 = number
 #Find the fraction of the number and assing to a variable called 'answer'.
-answer = number % 2
+answer = number // 2
 #Print the answer.
 print(answer)
 #Print the number.
