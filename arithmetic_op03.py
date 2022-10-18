d9901248d83ae6b1265f0d68638ee15beb734a62
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-10-18 16:22:15 +0500

    uch

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 83
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer = number % 10 * 10 + number // 10
+print(answer)
 #Print the answer variable
